f77d998a429f99edf8d41e5ea385e1113dd94c17
martin.vach <martin.vach@helmsauer-gruppe.de>
2024-06-24 16:02:26 +0200

    Adding sccs

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,12 +5,23 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <title>{{ $title ?? 'Page Title' }}</title>
-        @vite('resources/css/app.css')
+
+        @vite(['resources/scss/app.scss', 'resources/js/app.js'])
     </head>
     <body>
     <h1 class="text-3xl font-bold underline">
         Hello world!
     </h1>
+    <div class="container-cu mx-auto">
         {{ $slot }}
+    </div>
+    <div x-data="{ open: false }">
+        <button @click="open = true">Show More...</button>
+
+        <ul x-show="open" @click.outside="open = false">
+            <li><button wire:click="archive">Archive</button></li>
+            <li><button wire:click="delete">Delete</button></li>
+        </ul>
+    </div>
     </body>
 </html>
