6965514c18122850fc6ec8665392f1e83283faa7
martin.vach <martin.vach@helmsauer-gruppe.de>
2024-06-24 10:31:55 +0200

    #: Adding Livewire and Vite

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
-
+use App\Livewire\Web\Home;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
+Route::get('/', Home::class);
+Route::get('/test', function () {
     return view('welcome');
 });
