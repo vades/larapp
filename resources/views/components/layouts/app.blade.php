8d9b79180bd80ada87abae5259fc8dd30e8a2e5f
martin.vach <martin.vach@helmsauer-gruppe.de>
2024-06-26 08:15:57 +0200

    #1: Adding basic layout elements

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,25 +3,37 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+        <meta name="Description" content="{{ $description ?? 'This is LARAPP' }}">
         <title>{{ $title ?? 'LARAPP' }}</title>
 
         @vite(['resources/scss/app.scss', 'resources/js/app.js'])
     </head>
     <body>
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
-    <div>
-        {{ $slot }}
-    </div>
-    <div x-data="{ open: false }">
-        <button @click="open = true">Show More...</button>
+    <div class="flex flex-col h-screen justify-between">
+        <header class="bg-skin-header">
+            <section class="flex justify-between items-center container mx-auto p-3 md:p-4">
+                <div>app-header-brand</div>
+                <div>pp-header-nav</div>
+            </section>
+        </header>
 
-        <ul x-show="open" @click.outside="open = false">
-            <li><button wire:click="archive">Archive</button></li>
-            <li><button wire:click="delete">Delete</button></li>
-        </ul>
+        <main class="mb-auto container mx-auto p-3 md:pt-4 md:pb-4">
+            {{ $slot }}
+        </main>
+        <section>
+
+            <div class="container mx-auto p-4">
+                <div>app-footer-supplementary </div>
+
+            </div>
+        </section>
+        <footer class="bg-skin-footer border-t border-skin-footer">
+            <section class="container mx-auto text-center text-sm p-4 md:flex justify-between items-center text-skin-footer">
+                <div>app-footer-copyright</div>
+                <div>app-footer-nav</div>
+            </section>
+        </footer>
     </div>
+
     </body>
 </html>
