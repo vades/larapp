615eb19f1ca27b72b4ce83d565c791dcc4f34e8c
martin.vach <martin.vach@helmsauer-gruppe.de>
2024-07-08 16:21:19 +0200

    #1: Adding logo

--- resources/views/livewire/web/layout/header/brand.blade.php
+++ resources/views/livewire/web/layout/header/brand.blade.php
@@ -1,3 +1,3 @@
-<div>
-   Logo
+<div class="flex items-center gap-2">
+    <livewire:img.svg filename="logo" classList="[&>*]:w-8"/> AppName
 </div>
--- resources/views/livewire/web/layout/header/index.blade.php
+++ resources/views/livewire/web/layout/header/index.blade.php
@@ -1,7 +1,6 @@
 <header class="bg-skin-header">
     <section class="flex justify-between items-center container mx-auto p-3 md:p-4">
-        <livewire:img.svg filename="logo" classList="[&>*]:w-10"/>
-        {{--<livewire:web.layout.header.brand />--}}
+        <livewire:web.layout.header.brand />
         <livewire:web.layout.header.nav />
     </section>
 </header>
