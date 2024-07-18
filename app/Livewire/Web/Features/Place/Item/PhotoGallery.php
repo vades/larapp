461086d58842148b263c3a295822422fec4cb1b1
Martin Vach <vades.cz@gmail.com>
2024-07-18 20:42:15 +0200

    #4: Generating place item components

---
+++ app/Livewire/Web/Features/Place/Item/PhotoGallery.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Web\Features\Place\Item;
+
+use Livewire\Component;
+
+class PhotoGallery extends Component
+{
+    public function render()
+    {
+        return view('livewire.web.features.place.item.photo-gallery');
+    }
+}
