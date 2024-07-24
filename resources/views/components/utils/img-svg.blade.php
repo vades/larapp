5f70b4c959a36e1ea6e92d4080d498f80ad2e2ad
martin.vach <martin.vach@helmsauer-gruppe.de>
2024-07-24 15:35:08 +0200

    #5: Fixing image svg

--- resources/views/components/utils/img-svg.blade.php
+++ resources/views/components/utils/img-svg.blade.php
@@ -1,5 +1,10 @@
+@php
+
+    $img = $img ?? '';
+   $fullPath = 'assets/img/svg/' . $img . '.svg';
+@endphp
 <span class="{{$classList ?? ''}}">
-@if(file_exists($img))
-        {!! file_get_contents($img) !!}
+@if(file_exists($fullPath))
+        {!! file_get_contents($fullPath) !!}
     @endif
 </span>
