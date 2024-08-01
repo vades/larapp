2d3d3b6b7358278f819b5bd1316cca03dd7b7727
martin.vach <martin.vach@helmsauer-gruppe.de>
2024-08-01 15:51:49 +0200

    #5: Removing components from utils

--- resources/views/web/features/blog/blog-list.blade.php
+++ resources/views/web/features/blog/blog-list.blade.php
@@ -2,7 +2,7 @@
     <x-web.partials.page-header :page="$page" />
     <section class="sm:grid sm:grid-cols-2 sm:gap-2 md:gap-3 md:grid-cols-3 lg:grid-cols-4">
         @foreach($posts as $post)
-            <x-utils.card>
+            <x-card>
                 <x-slot name="header">
                     <img class="mr-auto ml-auto" src="{{$post->imageUrl}}" alt="{{ $post->title}}">
                 </x-slot>
@@ -15,8 +15,8 @@
                 <x-slot name="footer">
                     <a href="" class="btn btn-primary">Read More</a>
                 </x-slot>
-            </x-utils.card>
+            </x-card>
         @endforeach
     </section>
-    <x-utils.pagination />
+    <x-pagination />
 </x-web.layout>
