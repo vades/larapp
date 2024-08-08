a1a613e934a8fbbd2273475efcfeaf1847a922d6
martin.vach <martin.vach@helmsauer-gruppe.de>
2024-08-08 15:17:33 +0200

    #5: Adding utils page header

--- resources/views/components/web/features/blog/item/blog-item.blade.php
+++ resources/views/components/web/features/blog/item/blog-item.blade.php
@@ -1,9 +1,24 @@
 <x-web.layout :title="$post->metaTitle ?? null" :description="$post->metaDescription ?? null" :keywords="$post->keywords ?? null">
-    <x-web.partials.page-header :page="$post" />
-    <div class="text-sm mb-3">
-        <span class="mr-2">{{implode(', ', $post->authors)}}</span>
-        <span class="posts-date">{{ \Carbon\Carbon::parse($post->createdAt)->format('Y-m-d') }}</span>
-    </div>
+    <x-utils.page-header>
+        <x-slot name="image">
+            <img class="md:max-w-xs border-4 border-skin-muted drop-shadow-lg"
+                 src="{{$post->imageUrl}}"
+                 alt="{{ $post->title }}">
+        </x-slot>
+        <x-slot name="title">
+            {{ $post->title }}
+        </x-slot>
+        <x-slot name="subtitle">
+            {{ $post->subTitle ?? null }}
+        </x-slot>
+        <x-slot name="description">
+            {{ $post->description ?? null }}
+        </x-slot>
+        <x-slot name="info">
+            <span class="mr-2">{{implode(', ', $post->authors)}}</span>
+            <span class="posts-date">{{ \Carbon\Carbon::parse($post->createdAt)->format('Y-m-d') }}</span>
+        </x-slot>
+    </x-utils.page-header>
     <article>
         {!! $post->content !!}
     </article>
