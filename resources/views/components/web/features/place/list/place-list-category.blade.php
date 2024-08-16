e53380110d183e86da9e25a50478635e4441f45e
martin.vach <martin.vach@helmsauer-gruppe.de>
2024-08-16 12:12:44 +0200

    #5: Implementing place category view

--- resources/views/components/web/features/place/list/place-list-category.blade.php
+++ resources/views/components/web/features/place/list/place-list-category.blade.php
@@ -1,4 +1,28 @@
 <x-web.layout :title="$page->metaTitle" :description="$page->metaDescription" :keywords="$page->keywords">
     <x-web.partials.page-header :page="$page" />
+    <section class="sm:grid sm:grid-cols-2 sm:gap-2 md:gap-3 md:grid-cols-3 lg:grid-cols-4">
+        @foreach($categories as $category)
+            <a href="{{ route('placeList', ['category' => $category->slug]) }}">
+                <x-utils.card class="bg-skin-base">
+                    <x-slot name="header">
+                        <img class="mr-auto ml-auto"
+                             src="{{$category->imageUrl}}"
+                             alt="{{ $category->title}}">
+                    </x-slot>
+                    <x-slot name="body" class="p-3">
+                        <h2 class="text-2xl font-bold mb-3">{{ $category->title }}</h2>
+                        <div class="mb-3">
+                            {{ $category->description }}
+                        </div>
+                    </x-slot>
+                    <x-slot name="footer" class="p-3">
+                        <div>
+                            <span class="btn btn-primary">Show ({{ $category->numOfPosts }}) places</span>
+                        </div>
+                    </x-slot>
+                </x-utils.card>
+            </a>
 
+        @endforeach
+    </section>
 </x-web.layout>
