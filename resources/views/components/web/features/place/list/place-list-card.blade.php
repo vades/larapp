3f314e9be26102abbbf8bfff50401d370dd4a549
martin.vach <martin.vach@helmsauer-gruppe.de>
2024-08-16 12:05:27 +0200

    #5: Implementing place list view

--- resources/views/components/web/features/place/list/place-list-card.blade.php
+++ resources/views/components/web/features/place/list/place-list-card.blade.php
@@ -1,3 +1,18 @@
-<div>
-    <!-- You must be the change you wish to see in the world. - Mahatma Gandhi -->
-</div>
+<x-utils.card class="bg-skin-base">
+    <x-slot name="header">
+        <img class="mr-auto ml-auto"
+             src="{{$place->imageUrl}}"
+             alt="{{ $place->title}}">
+    </x-slot>
+    <x-slot name="body" class="p-3">
+        <h2 class="text-2xl font-bold mb-3">{{ $place->title }}</h2>
+        <div class="mb-3">
+            {{ $place->description }}
+        </div>
+    </x-slot>
+    <x-slot name="footer" class="p-3">
+        <div>
+            <span class="btn btn-primary">Read More</span>
+        </div>
+    </x-slot>
+</x-utils.card>
