438edc4f1d68618e17f7724cbd1c4ca9c038be9b
martin.vach <martin.vach@helmsauer-gruppe.de>
2024-09-18 15:40:56 +0200

    #6: Adding relations to post and projects

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\Post;
 class Project extends Model
 {
     use HasFactory;
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
 }
