f0a84d9cdc410d4d8d400700c08e9a72a1495070
martin.vach <martin.vach@helmsauer-gruppe.de>
2024-09-27 13:38:54 +0200

    #8: Generate services

---
+++ app/Services/Import/PostService.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+
+namespace App\Services\Import;
+
+class PostService
+{
+    # region PROPERTIES
+    # endregion PROPERTIES
+
+    # region CONSTRUCTOR_AND_DEPENDENCIES
+    public function __construct() {}
+    # endregion CONSTRUCTOR_AND_DEPENDENCIES
+
+    # region PROPERTY_ACCESSORS
+    # endregion PROPERTY_ACCESSORS
+
+    # region METHODS
+    # endregion METHODS
+}
