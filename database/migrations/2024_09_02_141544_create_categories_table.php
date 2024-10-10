026a3675d7f073bae1e5cf1097f8fac17688ac96
martin.vach <martin.vach@helmsauer-gruppe.de>
2024-10-10 14:23:07 +0200

    #8: Adding UUIDs to posts and cstegories

--- database/migrations/2024_09_02_141544_create_categories_table.php
+++ database/migrations/2024_09_02_141544_create_categories_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('uuid', 64)->unique();
             $table->foreignId('project_id')->default(0);
             $table->bigInteger('parent_id')->default(0);
             $table->boolean('is_published')->default(0);
