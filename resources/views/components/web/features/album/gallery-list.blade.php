6be3abdb539f0cf5831fdf3a97f1d6a7e3987578
martin.vach <martin.vach@helmsauer-gruppe.de>
2024-11-20 15:55:54 +0100

    #10: Adding carousel logic

--- resources/views/components/web/features/album/gallery-list.blade.php
+++ resources/views/components/web/features/album/gallery-list.blade.php
@@ -1,61 +1,70 @@
 <x-web.layout :title="$page->metaTitle" :description="$page->metaDescription" :keywords="$page->keywords">
     <x-web.partials.page-header :page="$page" />
-    <section class="grid grid-cols-2 gap-2 md:grid-cols-4 lg:grid-cols-5">
-        @foreach(collect($images) as $item)
-            <x-utils.card class="bg-skin-base">
-                <x-slot name="header">
-                    <img class="mr-auto ml-auto"
-                         src="{{$item->src}}"
-                         alt="{{ $item->title}}">
-                </x-slot>
-            </x-utils.card>
-        @endforeach
-    </section>
-
-
-
-    <div id="controls-carousel" class="relative w-full" data-carousel="static">
-        <!-- Carousel wrapper -->
-        <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
-            <!-- Item 1 -->
-            <div class="hidden duration-700 ease-in-out" data-carousel-item x-init="$el.classList.remove('hidden')">
-                <img src="https://dev.ivnbg.com/albums/nuremberg/fabrikgut-hammer/large/01.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+    <section  x-data="{ activeIndex: 0, items: $el.querySelectorAll('[data-carousel-item]'), showLightbox: false }"
+              x-init="items[0].classList.remove('hidden')">
+        <article id="controls-carousel" class="relative w-full" x-show="showLightbox">
+            <!-- Carousel wrapper -->
+            <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+                @foreach(collect($images) as $item)
+                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                        <img src="{{$item->src}}" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                             alt="{{ $item->title}}">
+                    </div>
+                @endforeach
             </div>
-            <!-- Item 2 -->
-            <div class="hidden duration-700 ease-in-out" data-carousel-item="active">
-                <img src="https://dev.ivnbg.com/albums/nuremberg/fabrikgut-hammer/large/02.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-            </div>
-            <!-- Item 3 -->
-            <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="https://dev.ivnbg.com/albums/nuremberg/fabrikgut-hammer/large/03.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-            </div>
-            <!-- Item 4 -->
-            <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="https://dev.ivnbg.com/albums/nuremberg/fabrikgut-hammer/large/04.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-            </div>
-            <!-- Item 5 -->
-            <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="https://dev.ivnbg.com/albums/nuremberg/fabrikgut-hammer/large/05.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-            </div>
-        </div>
-        <!-- Slider controls -->
-        <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+            <!-- Slider controls -->
+            <button type="button"
+                    class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+                    data-carousel-prev
+                    @click="items[activeIndex].classList.add('hidden'); activeIndex = (activeIndex - 1 + items.length) % items.length; items[activeIndex].classList.remove('hidden')">
         <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
+            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
+                 aria-hidden="true"
+                 xmlns="http://www.w3.org/2000/svg"
+                 fill="none"
+                 viewBox="0 0 6 10">
+                <path stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M5 1 1 5l4 4" />
             </svg>
             <span class="sr-only">Previous</span>
         </span>
-        </button>
-        <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+            </button>
+            <button type="button"
+                    class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+                    data-carousel-next
+                    @click="items[activeIndex].classList.add('hidden'); activeIndex = (activeIndex + 1) % items.length; items[activeIndex].classList.remove('hidden')">
         <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
+                 aria-hidden="true"
+                 xmlns="http://www.w3.org/2000/svg"
+                 fill="none"
+                 viewBox="0 0 6 10">
+                <path stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="m1 9 4-4-4-4" />
             </svg>
             <span class="sr-only">Next</span>
         </span>
-        </button>
-    </div>
+            </button>
+        </article>
+        <article class="grid grid-cols-2 gap-2 md:grid-cols-4 lg:grid-cols-5">
+            @foreach(collect($images) as $index => $item)
+                <x-utils.card class="bg-skin-base">
+                    <x-slot name="header">
+                        <img class="mr-auto ml-auto image-thumbnail"
+                             src="{{$item->thumbnail}}"
+                             alt="{{ $item->title}}"
+                             @click="showLightbox = true,items[activeIndex].classList.add('hidden'); activeIndex = {{ $index }}; items[activeIndex].classList.remove('hidden')">
+                    </x-slot>
+                </x-utils.card>
+            @endforeach
+        </article>
 
 
+    </section>
 </x-web.layout>
