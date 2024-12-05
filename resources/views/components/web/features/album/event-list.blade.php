b6c12dd3554f09c8b2080ad6f3a1c80a3e97ca4d
martin.vach <martin.vach@helmsauer-gruppe.de>
2024-12-05 15:25:53 +0100

    develop: Adjusting album styles

--- resources/views/components/web/features/album/event-list.blade.php
+++ resources/views/components/web/features/album/event-list.blade.php
@@ -2,8 +2,8 @@
     <x-web.partials.page-header :page="$page" />
     <section class="grid grid-cols-2 gap-2 md:grid-cols-4 lg:grid-cols-5">
     @foreach(collect($events) as $item)
-            <a href="{{ route('albumEventList', $item->albumId) }}/{{$item->id}}">
-                <x-utils.card class="bg-skin-base">
+            <a class="text-skin-album" href="{{ route('albumEventList', $item->albumId) }}/{{$item->id}}">
+                <x-utils.card class="bg-skin-album">
                     <x-slot name="header">
                         <img class="mr-auto ml-auto"
                              src="{{$item->src}}"
