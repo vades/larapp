6a466a272bf97a9e95a5bbe68e8c70f71504bfc6
martin.vach <martin.vach@helmsauer-gruppe.de>
2024-12-06 11:22:00 +0100

    develop: Adjusting prev next styles

--- resources/views/components/utils/prev-next.blade.php
+++ resources/views/components/utils/prev-next.blade.php
@@ -4,38 +4,16 @@
         <!-- region previous  -->
         @if(isset($prevUrl))
             <a href="{{$prevUrl}}"
-               class="flex items-center justify-center px-4 h-10 me-3 text-base font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-                <svg class="w-3.5 h-3.5 me-2 rtl:rotate-180"
-                     aria-hidden="true"
-                     xmlns="http://www.w3.org/2000/svg"
-                     fill="none"
-                     viewBox="0 0 14 10">
-                    <path stroke="currentColor"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M13 5H1m0 0 4 4M1 5l4-4" />
-                </svg>
-                Previous
+               class="button before:content-['\2039'] before::ml-2 rtl:after:rotate-180">
+                Prev
             </a>
         @endif
         <!-- endregion -->
         <!-- region next  -->
         @if(isset($nextUrl))
             <a href="{{$nextUrl}}"
-               class="flex items-center justify-center px-4 h-10 text-base font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+               class="button after:content-['\203A'] after:ml-2 rtl:after:rotate-180">
                 Next
-                <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180"
-                     aria-hidden="true"
-                     xmlns="http://www.w3.org/2000/svg"
-                     fill="none"
-                     viewBox="0 0 14 10">
-                    <path stroke="currentColor"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M1 5h12m0 0L9 1m4 4L9 9" />
-                </svg>
             </a>
         @endif
         <!-- endregion -->
