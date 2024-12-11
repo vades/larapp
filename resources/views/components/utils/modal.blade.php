bb3495f50815af034e8720898f8fc4b5956b3e6c
martin.vach <martin.vach@helmsauer-gruppe.de>
2024-12-11 15:36:22 +0100

    develop: Adjusting lightbox for small devices

--- resources/views/components/utils/modal.blade.php
+++ resources/views/components/utils/modal.blade.php
@@ -5,7 +5,7 @@
     <div class="relative top-10 mx-auto max-w-[1800px] shadow-xl bg-white h-[90%] dark:bg-gray-700">
         <header class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
             <div class="text-xl font-semibold text-gray-900 dark:text-gray-400">{{$title ?? ''}}</div>
-            <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" @click="close()">
+            <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" @click="closeModal()">
                 <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                 </svg>
@@ -29,18 +29,18 @@
     document.addEventListener('alpine:init', () => {
         Alpine.data('modalControl', () => ({
             showModal: false,
-            open() {
-                this.showModal = true;
-                document.body.classList.add('overflow-y-hidden');
+            openModal() {
+                this.showModal = !this.showModal;
+                document.body.classList.toggle('overflow-y-hidden');
                 document.getElementById('modalWindow').classList.remove('hidden');
             },
-            close() {
+            closeModal() {
                 this.showModal = false;
                 document.body.classList.remove('overflow-y-hidden');
             },
-            closeOnEscape(event) {
+            closeModalOnEscape(event) {
                 if (event.keyCode === 27) {
-                    this.close();
+                    this.closeModal();
                 }
             }
         }));
